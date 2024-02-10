ebebf2cd3d6392e84cdf27a5b871e1d2afe17fcc
PhFrilles <88896188+PhFrilles@users.noreply.github.com>
2024-02-10 12:57:24 +0000

    First commit of DKBase.py
    This should be considered the main branch.
    10/02/2024
    
    LOG:
    
    # TO DO:
    # > BE ABLE TO VIEW/PULL RECORDS. POSSIBLY EDIT FIELDS OF A RECORD
    
    
    # ISSUES:
    # -Some field lengths are dictated by the size of their heading/title.
    #  >> This allows age to be 3 digits (100+) when it was specified only to be 2. Will require validation check elsewhere.
    #  >> Field length values are not even used for validation, its only used for formatting.
    #  >> Inconsistent? Formatting may change depending on different inputted field headings and lengths.
    #
    # -Doesnt actually check the data type of each fields. Converts all data into str to be written onto the txt file.
    #
    # -When creating a DB, duplicate fields with the same heading can be made.
    #
    # -Function 'add_records()' error checking can be optimised. too many for loops. Can be done under a single for loop.

---
+++ DKBase.py
@@ -0,0 +1,333 @@
+# DeeKie Base Manager (Data base manager)
+# - Started 28 October 2023
+# -------------------------------------------
+import os
+
+os.chdir('D:\\Desktop\\python_folder\\Year 13 - NEA stuff\\database_manager\\databases')
+
+
+class DKBase:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_exists = True
+
+        try:
+            with open(self.file_name, 'r') as f:  # Checks if file already exists
+                pass
+        except FileNotFoundError:
+            self.file_exists = False
+
+    # --------------------------------------------------- # USE THIS FUNCTION ONLY TO READ AND GET DATA
+    def open(self):
+        if self.file_exists:
+            pass
+        else:
+            error_message = "ERROR: file '{}' does not exist.".format(self.file_name)  # Error checking
+            raise Exception(error_message)                                             # Checks if file even exists
+
+    # ----------------------------------------------------
+    def create(self, fields):  # fields MUST BE A DICTIONARY
+        if self.file_exists:
+            error_message = "ERROR: file '{}' already exists.".format(self.file_name)  # Error checking
+            raise Exception(error_message)                                             # Checks if file already exists
+        else:                                                                          # Prevents overwriting
+            pass
+
+        try:
+            for i in fields.values():                                # Error checking
+                int(i)                                               # Checks if value in dict is a string
+        except ValueError:
+            raise Exception("ERROR: invalid type for key values")
+
+        headings_row = ""
+        heading_string = ""
+
+        for field_title in fields:  # Formatting field titles to be written onto txt files
+            field_title_length = len(field_title)
+            field_length = fields[field_title]
+
+            if field_title_length >= field_length:
+                heading_string = field_title + "|"
+
+            elif field_title_length < field_length:
+                string = field_title.center(field_length)  # Sets and formats field length to correct length.
+                heading_string = string + "|"  # Puts field title in middle, between whitespace.
+
+            headings_row = headings_row + heading_string
+
+        print(headings_row)
+
+        with open(self.file_name, 'w') as file:
+            formatted_headings_row = headings_row + '\n'
+            file.write(formatted_headings_row)
+            print("File created successfully")
+
+    # ------------------------------------------------------
+    def add_records(self, *records):  # records MUST BE A TUPLE
+
+        def find_index_positions():
+            index_counter = -1
+            character_count = 0
+            positions = []                                 # Function to find the index positions of '|'
+
+            for char in first_line:
+                index_counter += 1
+                if char == '|':
+                    character_count += 1
+                    positions.append(index_counter)
+
+            return positions
+
+        def get_field_lengths():
+            lengths_list = []
+            index_counter = -1
+
+            for num in index_positions_list:
+                index_counter += 1
+
+                if index_counter == 0:
+                    lengths_list.append(num)  # Gets first field length
+                else:
+                    length = num - index_positions_list[index_counter - 1]  # Gets remaining field lengths
+                    lengths_list.append(length - 1)
+
+            return lengths_list
+
+        first_line = ""
+
+        if not isinstance(records, tuple):  # Error checking
+            raise Exception("ERROR: invalid records argument given. Must be a tuple.")  # Checks if records is a tuple.
+
+        with open(self.file_name, 'r') as file:  # Gets headings row of DB for format checks e.g. fields given.
+            lines = file.readlines()
+            first_line = lines[0]
+
+        number_of_fields = first_line.count('|')
+
+        for record in records:  # Error checking
+            if not isinstance(record, tuple):  # Checks if record is a tuple
+                raise Exception("ERROR: invalid records argument given. Must be a tuple.")
+
+        for record in records:  # Error checking
+            if len(record) != number_of_fields:  # Checks if record has correct amount of fields.
+                raise Exception("ERROR: one or more records have an invalid length.")
+
+        index_positions_list = find_index_positions()
+        field_lengths_list = get_field_lengths()
+
+        for record in records:
+            index_counter = -1  # Error checking
+            for data in record:  # Checks if each data given exceeds their respective field length.
+                index_counter += 1
+                if len(str(data)) > field_lengths_list[index_counter]:
+                    raise Exception("ERROR: one or more given fields have exceeded the field length")
+
+        for record in records:
+            record_string = ""
+            index_counter = -1  # Formatting the records to be written into the txt file.
+            for data in record:
+                index_counter += 1
+                data_string = str(data).center(field_lengths_list[index_counter])
+                data_string = data_string + '|'
+                record_string += data_string
+
+            print("Record string: ", record_string)
+
+            with open(self.file_name, 'a') as file:
+                formatted_record_string = record_string + "\n"
+                file.write(formatted_record_string)
+
+    def del_records(self, conditions):  # conditions MUST BE A DICTIONARY
+
+        def find_index_positions():
+            index_counter = -1
+            character_count = 0
+            positions = []                               # Function to find the index positions of '|'
+
+            for char in headings_row:
+                index_counter += 1
+                if char == '|':
+                    character_count += 1
+                    positions.append(index_counter)
+
+            return positions
+
+        condition_fields = list(conditions)
+        #print(condition_fields)
+
+        if not isinstance(conditions, dict):                                                    # Error checking
+            raise Exception("ERROR: invalid conditions argument given. Must be a dictionary.")  # Checks if its a dict
+
+        with open(self.file_name, 'r') as file:  # Gets headings row of DB for format checks e.g. fields given.
+            file_lines = file.readlines()
+            headings_row = file_lines[0]
+
+        for field in condition_fields:
+            if field not in headings_row:                                                # Error checking
+                raise Exception("ERROR: Fields given do not exist in the database.")     # Checks if fields exist in DB
+
+        index_positions = [0]
+        for pos in find_index_positions():    # Gets list of index positions of '|', inserting 0 at the start.
+            index_positions.append(pos)
+
+        field_counter = -1
+        lines_to_be_removed = []
+        for field in condition_fields:
+            field_counter += 1
+            field_index_pos = headings_row.index(field)
+
+            if field_index_pos == 0:
+                column_start_pos = 0                   # Gets index positions of '|' for the first column/field
+                column_end_pos = index_positions[1]
+            else:                                       # VVV   SEE NOTES FOR VISUAL AND BETTER EXPLANATION   VVV
+                index_positions_for_search = []
+                for i in index_positions:               # Makes temporary list of index positions to insert the index
+                    index_positions_for_search.append(i)  # pos of the field.
+
+                index_positions_for_search.append(field_index_pos)  # Sorts it, putting field pos between the index
+                index_positions_for_search.sort()  # positions of the starting '|' and the ending '|'.
+
+                field_index_pos_for_search = index_positions_for_search.index(field_index_pos)
+
+                column_start_pos = index_positions[field_index_pos_for_search-1]  # Gives us index positions of '|'
+                column_end_pos = index_positions[field_index_pos_for_search]  # for the field/column
+
+            #print("Start pos:", column_start_pos)
+            #print("End pos:", column_end_pos)
+            #print(" ")
+
+            line_index_counter = -1
+            for line in file_lines:
+                line_index_counter += 1
+                data = line[column_start_pos:column_end_pos]
+                current_field = condition_fields[field_counter]
+                condition = str(conditions[current_field])
+                #print("Condition:", condition)
+
+                if column_start_pos == 0:
+                    stripped_data = data.strip()
+                else:                                         # Gets data from text file, removes whitespace.
+                    data = data[1:]                           # Just gets the data
+                    stripped_data = data.strip()
+
+                #print("Stripped data:", stripped_data)
+
+                if condition == stripped_data:
+                    lines_to_be_removed.append(line_index_counter)
+                    #print(line)
+
+        #print("Lines to be removed:", lines_to_be_removed)
+        lines_to_be_removed = list(set(lines_to_be_removed))  # Removes any duplicates from list
+        #print("Lines to be removed:", lines_to_be_removed)
+
+        shift_counter = -1
+        for num in lines_to_be_removed:
+            shift_counter += 1                                # Removes line from list of file lines.
+            removed_line = num - shift_counter                # Shift counter used as index positions change when a line
+            file_lines.pop(removed_line)                      # is removed. Shift counter remembers what line to remove.
+
+        with open(self.file_name, 'w') as file:
+            for line in file_lines:                           # Overwrites file with the changes made.
+                file.write(line)
+
+
+
+    # ------------------------------------------------------------------------
+    def view(self, conditions):
+        def find_index_positions():
+            index_counter = -1
+            character_count = 0
+            positions = []  # Function to find the index positions of '|'
+
+            for char in headings_row:
+                index_counter += 1
+                if char == '|':
+                    character_count += 1
+                    positions.append(index_counter)
+
+            return positions
+
+        condition_fields = list(conditions)
+        # print(condition_fields)
+
+        if not isinstance(conditions, dict):  # Error checking
+            raise Exception("ERROR: invalid conditions argument given. Must be a dictionary.")  # Checks if its a dict
+
+        with open(self.file_name, 'r') as file:  # Gets headings row of DB for format checks e.g. fields given.
+            file_lines = file.readlines()
+            headings_row = file_lines[0]
+
+        for field in condition_fields:
+            if field not in headings_row:  # Error checking
+                raise Exception("ERROR: Fields given do not exist in the database.")  # Checks if fields exist in DB
+
+        index_positions = [0]
+        for pos in find_index_positions():  # Gets list of index positions of '|', inserting 0 at the start.
+            index_positions.append(pos)
+
+        field_counter = -1
+        lines_to_be_read = []
+        for field in condition_fields:
+            field_counter += 1
+            field_index_pos = headings_row.index(field)
+
+            if field_index_pos == 0:
+                column_start_pos = 0  # Gets index positions of '|' for the first column/field
+                column_end_pos = index_positions[1]
+            else:  # VVV   SEE NOTES FOR VISUAL AND BETTER EXPLANATION   VVV
+                index_positions_for_search = []
+                for i in index_positions:  # Makes temporary list of index positions to insert the index
+                    index_positions_for_search.append(i)  # pos of the field.
+
+                index_positions_for_search.append(field_index_pos)  # Sorts it, putting field pos between the index
+                index_positions_for_search.sort()  # positions of the starting '|' and the ending '|'.
+
+                field_index_pos_for_search = index_positions_for_search.index(field_index_pos)
+
+                column_start_pos = index_positions[field_index_pos_for_search - 1]  # Gives us index positions of '|'
+                column_end_pos = index_positions[field_index_pos_for_search]  # for the field/column
+
+            # print("Start pos:", column_start_pos)
+            # print("End pos:", column_end_pos)
+            # print(" ")
+
+            line_index_counter = -1
+            for line in file_lines:
+                line_index_counter += 1
+                data = line[column_start_pos:column_end_pos]
+                current_field = condition_fields[field_counter]
+                condition = str(conditions[current_field])
+                # print("Condition:", condition)
+
+                if column_start_pos == 0:
+                    stripped_data = data.strip()
+                else:  # Gets data from text file, removes whitespace.
+                    data = data[1:]  # Just gets the data
+                    stripped_data = data.strip()
+
+                # print("Stripped data:", stripped_data)
+
+                if condition == stripped_data:
+                    lines_to_be_read.append(line_index_counter)
+                    # print(line)
+
+        # print("Lines to be read:", lines_to_be_read)
+        # lines_to_be_read = list(set(lines_to_be_read))  # Removes any duplicates from list
+        # print("Lines to be read:", lines_to_be_read)
+
+        for pos in lines_to_be_read:
+            print(file_lines[pos])
+
+        # NOT FINISHED - CANNOT VIEW THE WHOLE TABLE
+
+
+# TESTS --------------------------------
+
+database = DKBase('bookings - Copy.txt')
+database.open()
+
+database.view(
+    {'Seating': 'Outdoor'}
+)
+
+# TESTS --------------------------------
